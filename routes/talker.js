fc866db85f83c8e72f42e45460cb5edb17a5f5cb
yMaatheus <stvgamer74@gmail.com>
2022-06-23 18:14:18 -0300

    feat: aplicando a validações de token e no talker, escrevendo novo talker no arquivo e informando na resposta da requisição

--- routes/talker.js
+++ routes/talker.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const rescue = require('express-rescue');
+
 const { HTTP_OK, HTTP_404 } = require('../helpers/httpCodeStatus');
+const validateToken = require('../middlewares/tokenMiddleware');
+const { validateTalker } = require('../middlewares/talkerMiddleware');
 
 const router = express.Router();
 
 const readFile = require('../helpers/readFile');
+const writeFile = require('../helpers/writeFile');
 
 router.get('/', rescue(async (_req, res) => {
   const talkers = await readFile('./talker.json');
   res.status(HTTP_OK).json(talkers);
+}))
+.post('/', validateToken, validateTalker, rescue(async (req, res) => {
+  const { name, age, talk } = req.body;
+  const talkers = await readFile('./talker.json');
+  const newId = (talkers.reduce((acc, { id }) => acc < id && id, 0)) + 1;
+  const newTalker = { id: newId, name, age, talk };
+  const newTalkers = [...talkers, newTalker];
+  await writeFile('talker.json', JSON.stringify(newTalkers));
+  res.status(201).json(newTalker);
 }));
 
 router.get('/:id', rescue(async (req, res) => {
