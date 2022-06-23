c8a5cc05ff27710dac149db735456c7bfe21379a
yMaatheus <stvgamer74@gmail.com>
2022-06-23 06:15:58 -0300

    feat: criando rota que possui parametro id o qual retorna o palestrante igual ao id informado

--- routes/talker.js
+++ routes/talker.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const rescue = require('express-rescue');
-const { HTTP_OK_STATUS } = require('../helpers/httpCodeStatus');
+const { HTTP_OK, HTTP_404 } = require('../helpers/httpCodeStatus');
 
 const router = express.Router();
 
 const readFile = require('../helpers/readFile');
 
 router.get('/', rescue(async (_req, res) => {
-  const talker = await readFile('./talker.json');
-  res.status(HTTP_OK_STATUS).json(talker);
+  const talkers = await readFile('./talker.json');
+  res.status(HTTP_OK).json(talkers);
+}));
+
+router.get('/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile('./talker.json');
+  const talkerObj = talkers.find((talker) => +id === talker.id);
+
+  if (!talkerObj)res.status(HTTP_404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(HTTP_OK).json(talkerObj);
 }));
 
 module.exports = router;
