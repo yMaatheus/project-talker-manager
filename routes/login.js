1581677085556c90901f3eec1472c24a57184ce6
yMaatheus <stvgamer74@gmail.com>
2022-06-23 07:50:18 -0300

    feat(/login): escrevendo no arquivo token.json o token gerado na rota /login

--- routes/login.js
+++ routes/login.js
@@ -2,13 +2,15 @@ const express = require('express');
 const rescue = require('express-rescue');
 
 const randomToken = require('../helpers/token');
+const writeFile = require('../helpers/writeFile');
 const { HTTP_OK } = require('../helpers/httpCodeStatus');
 const { validateEmail, validatePassword } = require('../middlewares/loginMiddleware');
 
 const router = express.Router();
 
-router.post('/', validateEmail, validatePassword, rescue(async (req, res) => {
+router.post('/', validateEmail, validatePassword, rescue(async (_req, res) => {
     const token = randomToken();
+    await writeFile('../token.json', { token });
     res.status(HTTP_OK).json({ token });
 }));
 
