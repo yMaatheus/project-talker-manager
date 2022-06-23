1a93af86d18c2d684f9ef8e7755814914dc71b0e
yMaatheus <stvgamer74@gmail.com>
2022-06-23 19:07:51 -0300

    feat: criando rota de DELETE para excluir um palestrante com id especifico

--- routers/talker.js
+++ routers/talker.js
@@ -9,43 +9,55 @@ const router = express.Router();
 const readFile = require('../helpers/readFile');
 const writeFile = require('../helpers/writeFile');
 
+const TALKER_FILE = './talker.json';
+
 router.get('/', rescue(async (_req, res) => {
-  const talkers = await readFile('./talker.json');
+  const talkers = await readFile(TALKER_FILE);
   res.status(200).json(talkers);
 }))
 .post('/', validateToken, validateTalker, rescue(async (req, res) => {
   const { name, age, talk } = req.body;
-  const talkers = await readFile('./talker.json');
+  const talkers = await readFile(TALKER_FILE);
 
   const newId = (talkers.reduce((acc, { id }) => acc < id && id, 0)) + 1;
 
   const newTalker = { id: newId, name, age, talk };
   const newTalkers = [...talkers, newTalker];
 
-  await writeFile('talker.json', JSON.stringify(newTalkers));
+  await writeFile(TALKER_FILE, JSON.stringify(newTalkers));
 
   res.status(201).json(newTalker);
 }));
 
 router.get('/:id', rescue(async (req, res) => {
   const { id } = req.params;
-  const talkers = await readFile('./talker.json');
+  const talkers = await readFile(TALKER_FILE);
   const talker = talkers.find(({ id: talkerId }) => +id === talkerId);
 
   if (!talker)res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   res.status(200).json(talker);
-})).put('/:id', validateToken, validateTalker, rescue(async (req, res) => {
+}))
+.put('/:id', validateToken, validateTalker, rescue(async (req, res) => {
   const { id } = req.params;
   const { name, age, talk } = req.body;
 
-  const readTalkers = await readFile('./talker.json');
+  const readTalkers = await readFile(TALKER_FILE);
   const talkers = readTalkers.filter(({ id: talkerId }) => talkerId !== +id);
 
   const newTalker = { id: +id, name, age, talk };
   const newTalkers = [...talkers, newTalker];
 
-  await writeFile('talker.json', JSON.stringify(newTalkers));
+  await writeFile(TALKER_FILE, JSON.stringify(newTalkers));
   res.status(200).json(newTalker);
+}))
+.delete('/:id', validateToken, rescue(async (req, res) => {
+  const { id } = req.params;
+
+  const readTalkers = await readFile(TALKER_FILE);
+  const talkers = readTalkers.filter(({ id: talkerId }) => talkerId !== +id);
+
+  await writeFile(TALKER_FILE, JSON.stringify(talkers));
+  res.status(204).end();
 }));
 
 module.exports = router;
